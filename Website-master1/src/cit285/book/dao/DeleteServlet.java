1b9a4c51b2df66efa553fc6becbd4f957a3f66aa
Johnbel Mahautiere <47404011+johnbel458@users.noreply.github.com>
2019-11-21 20:11:39 -0500

    Add files via upload

---
+++ Website-master1/src/cit285/book/dao/DeleteServlet.java
@@ -0,0 +1,19 @@
+package cit285.book.dao;
+
+import java.io.IOException;  
+import javax.servlet.ServletException;  
+import javax.servlet.annotation.WebServlet;  
+import javax.servlet.http.HttpServlet;  
+import javax.servlet.http.HttpServletRequest;  
+import javax.servlet.http.HttpServletResponse;  
+@SuppressWarnings("serial")
+@WebServlet("/DeleteServlet")  
+public class DeleteServlet extends HttpServlet {  
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)   
+             throws ServletException, IOException {  
+        String sid=request.getParameter("id");  
+        int id=Integer.parseInt(sid);  
+        EmpDao.delete(id);  
+        response.sendRedirect("ViewServlet");  
+    }  
+}  
